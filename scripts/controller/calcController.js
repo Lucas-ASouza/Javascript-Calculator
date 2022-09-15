4109c2a1b1c8e61fe82dd71cd5cb1d79a8ce0a7e
Lucas-ASouza <lucasalves-x@hotmail.com>
2022-09-15 09:04:31 -0300

    Getters and Setters of the class

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,4 +1,27 @@
 class CalcController{
     
+    constructor(){
+        this._displayCalc = "0";
+        this._dataAtual;
+        /*This. é a forma de criação de um atributo, faz 
+        referência do objeto que está sendo instanciado.
+        adicionar "_" após o "." torna o atributo privado*/
 
+    }
+    get displayCalc(){
+
+        return this._displayCalc;
+        /*retorna atributo privado*/
+    }
+
+    set displayCalc(valor){
+        this.displayCalc = valor;
+        /* Define atributo privado*/
+    }
+    get dataAtual(){
+        return this._dataAtual;
+    }
+    set dataAtual(valor){
+        this.dataAtual = valor;
+    }
 }
