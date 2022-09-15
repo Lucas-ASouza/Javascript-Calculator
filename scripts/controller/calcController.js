f2546b1d78c49dc9b1658929c6ec521119e810eb
Lucas-ASouza <lucasalves-x@hotmail.com>
2022-09-15 12:39:39 -0300

    Time and Date now with realtime updates.

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,32 +1,76 @@
 class CalcController{
     
     constructor(){
-        this._displayCalc = "0";
+
+        this._locale = 'pt-BR';
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
+        /*Manipulando o DOM, elemento do documento da página Metodo 
+        querySelector serve para selecionar um elemento do DOM pela
+        sua id# e manipula-lo,*/
+        /*El no final está sendo usado como um sufixo, para 
+        padronizad quando estamos indicando de que vem de um 
+        lemento */
         this._currentDate;
+        this.initialize();
         /*This. é a forma de criação de um atributo, faz 
         referência do objeto que está sendo instanciado.
         adicionar "_" após o "." torna o atributo privado*/
 
     }
 
-    initialize (){
-    
+    initialize(){
+        
+        setInterval(() => {
+
+            this.displayDate = this.currentDate.toLocaleDateString(this.locale);
+            this.displayTime = this.currentDate.toLocaleTimeString(this.locale);
+        }, 1000);
 
     }
+ 
     get displayCalc(){
 
-        return this._displayCalc;
+        return this._displayCalcEl.innerHTML;
         /*retorna atributo privado*/
+              /* innerHTML é utilizara para selecionar 
+                um elemento e inserir uma informação 
+                nele no formato HTML */
     }
 
-    set displayCalc(valor){
-        this.displayCalc = valor;
+    set displayCalc(value){
+        this._displayCalcEl.innerHTML = value;
         /* Define atributo privado*/
     }
+
+
+    get displayTime(){
+        this._timeEl.innerHTML;
+    }
+
+
+    set displayTime(value){
+        return this._timeEl.innerHTML = value;
+    }
+
+
+    get displayDate(){
+        this._dateEl.innerHTML;
+    }
+
+
+    set displayDate(value){
+        return this._dateEl.innerHTML = value;
+    }
+
+
     get currentDate(){
-        return this._currentDate;
+        return new Date;
     }
-    set currentDate(valor){
-        this.currentDate = valor;
+
+    set currentDate(value){
+        this.currentDate = value;
     }
+
 }
